dc8069b2608ed705c3027a8b4aaadad842c7c2fa
Aar Khabar <khab0018@algonquinlive.com>
2021-11-18 01:07:03 -0500

    Finish design without app connection

--- app/src/main/java/algonquin/cst2335/finalprojectassignment/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalprojectassignment/MainActivity.java
@@ -3,6 +3,23 @@ package algonquin.cst2335.finalprojectassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
