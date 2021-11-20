2ff0bbafd91d8fdf669f233a7cdf648875844fb5
Aar Khabar <khab0018@algonquinlive.com>
2021-11-20 01:16:38 -0500

    save project

--- app/src/main/java/algonquin/cst2335/finalprojectassignment/OwlBotDictionary.java
+++ app/src/main/java/algonquin/cst2335/finalprojectassignment/OwlBotDictionary.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class OwlBotDictionary extends AppCompatActivity {
 
@@ -17,13 +20,24 @@ public class OwlBotDictionary extends AppCompatActivity {
         Button btn = findViewById(R.id.button);
         EditText edt = findViewById(R.id.editText);
 
-        btn.setOnClickListener( clk -> {
-            Context context = getApplicationContext();
-            CharSequence text = "Searching for " + edt.getText().toString();
-            int duration = Toast.LENGTH_SHORT;
 
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String read = edt.getText().toString();
+                if(read.isEmpty()){
+                    Snackbar.make(findViewById(R.id.snackbarlayout),"Search bar is empty",Snackbar.LENGTH_LONG).show();
+                }
+                else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "Searching for " + edt.getText().toString();
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
         });
+
     }
 }
